926f8f37dc126e39f072e637d5680e9ad6c9484d
Sebastien L deGroot <sebastiendegroot2@gmail.com>
2023-03-14 15:13:03 -0700

    batching requests

--- src/components/pdfuploader.tsx
+++ src/components/pdfuploader.tsx
@@ -1,15 +1,24 @@
-import { type Dispatch, type SetStateAction } from "react";
+import { type Dispatch, type SetStateAction, useState } from "react";
 import axios from "axios";
 import { FileUploader } from "react-drag-drop-files";
 import { Card } from "../ui/card";
 import UploaderBox from "./uploader-box";
 
-const FLASK_API_BASE = "https://pdfsummarizer-backend.herokuapp.com";
+// const FLASK_API_BASE = "https://pdfsummarizer-backend.herokuapp.com";
+const FLASK_API_BASE = "http://127.0.0.1:5000";
 
 const fileTypes = ["PDF"];
 
-interface Response {
-  summary: string;
+interface GetTextChunksResponse {
+  data: {
+    chunks: Array<string>;
+  };
+}
+
+interface SummaryResponse {
+  data: {
+    summary: string;
+  };
 }
 
 interface ComponentProps {
@@ -21,24 +30,48 @@ interface ComponentProps {
 const PDFUPloader = (props: ComponentProps) => {
   const { setSummary, setIsFileUploaded, setLoading } = props;
 
-  const axiosUploadHandler = (formData: FormData) => {
+  const axiosUploadHandler = async (formData: FormData) => {
+    let textChunks: Array<string> = [];
+
     const options = {
       method: "POST",
-      url: `${FLASK_API_BASE}/get_text`,
+      url: `${FLASK_API_BASE}/get_text_chunks`,
       data: formData,
     };
-    axios
-      .request(options)
-      .then(({ data }: { data: Response }) => {
-        setSummary(data.summary);
-        setLoading(false);
-      })
-      .catch((error: Error) => {
+    try {
+      const responseData: GetTextChunksResponse = await axios.request(options);
+      textChunks = textChunks.concat(responseData.data?.chunks);
+    } catch (error) {
+      setIsFileUploaded(false);
+      setLoading(false);
+      alert("Sorry, something went wrong!");
+      console.log(error);
+    }
+
+    const summaryPromises: Array<Promise<SummaryResponse>> = [];
+    textChunks.forEach((chunk) => {
+      const summaryOptions = {
+        method: "POST",
+        url: `${FLASK_API_BASE}/get_chunk_summary`,
+        data: { chunk: chunk },
+      };
+      try {
+        const summaryResponse = axios.request(summaryOptions);
+        summaryPromises.push(summaryResponse);
+      } catch (error) {
         setIsFileUploaded(false);
         setLoading(false);
-        alert("Sorry, something went wrong!");
+        alert("Sorry something went wrong when getting a summary");
         console.log(error);
-      });
+      }
+    });
+    const allSummaryResonses = await Promise.all(summaryPromises);
+    let fullSummary = "";
+    allSummaryResonses.forEach((summaryResponse: SummaryResponse) => {
+      fullSummary = fullSummary + summaryResponse.data.summary;
+    });
+    setSummary(fullSummary);
+    setLoading(false);
   };
 
   const handleUploadFile = (file: File) => {
@@ -48,7 +81,9 @@ const PDFUPloader = (props: ComponentProps) => {
       setLoading(true);
       formData.append("file", file, file.name);
       console.log("The selected file is ", file);
-      axiosUploadHandler(formData);
+      axiosUploadHandler(formData).catch((error: Error) => {
+        console.log("Error in axios handler", error);
+      });
     }
   };
 
--- src/components/summary-box.tsx
+++ src/components/summary-box.tsx
@@ -11,9 +11,17 @@ const SummaryBox = (props: ComponentProps) => {
   const { loading, summary } = props;
   return (
     <Card>
-      <div className="mb-5 flex justify-center text-xl">Summary</div>
+      <div className="mb-5 flex justify-center text-xl">
+        {loading ? (
+          <p>Generating summary. This may take a while.</p>
+        ) : (
+          <p>Summary</p>
+        )}
+      </div>
       {loading ? (
-        <Skeleton count={3} />
+        <div>
+          <Skeleton count={3} />
+        </div>
       ) : (
         <div className="text-l flex justify-center">{summary}</div>
       )}
